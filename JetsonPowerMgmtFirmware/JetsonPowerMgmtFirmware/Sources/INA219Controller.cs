8d7fb990a24f17b083176387a4a7e7c8b5bbf129
Hunter Robinson <robinsonhunterb@gmail.com>
2020-03-15 01:25:39 -0500

    Initial Commit
    Created NanoFramework VS Project
    Created GPIO PWR Control Class
    Created INA219 Control Class

---
+++ JetsonPowerMgmtFirmware/JetsonPowerMgmtFirmware/Sources/INA219Controller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Windows.Devices.I2c;
+
+namespace JetsonPowerMgmtFirmware
+{
+    public class INA219Controller
+    {
+        private readonly int INAStartAddress = 0x40;     //Jetson PWR MGMT Module starts from address 1000000 up to 1000005
+        public int INACount { get; set; } = 0;
+        public INA219Controller(string i2cBus)
+        {
+            bool IsRunning = true;
+            while (IsRunning)
+            {
+                int INAAddress = INAStartAddress;
+                I2cDevice currentINA;
+
+                currentINA = I2cDevice.FromId(i2cBus, new I2cConnectionSettings(INAAddress)
+                {
+                    BusSpeed = I2cBusSpeed.StandardMode,
+                    SharingMode = I2cSharingMode.Exclusive
+                });
+
+                INAAddress++;
+            }
+            
+        }
+    }
+}
---
+++ JetsonPowerMgmtFirmware/JetsonPowerMgmtFirmware/Sources/PWRController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace JetsonPowerMgmtFirmware
+{
+    class PWRController
+    {
+        public PWRController()
+        {
+
+        }
+    }
+}
