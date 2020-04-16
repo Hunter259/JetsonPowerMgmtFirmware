7e71b7e49be113f6c0ea1dd6782ddedb445f5166
Hunter Robinson <robinsonhunterb@gmail.com>
2020-04-16 18:03:09 -0500

    Fixed code style issues. Removed unused variable INACount. Changed readonly variables to proper const type.

--- JetsonPowerMgmtFirmware/JetsonPowerMgmtFirmware/Sources/INA219Controller.cs
+++ JetsonPowerMgmtFirmware/JetsonPowerMgmtFirmware/Sources/INA219Controller.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Text;
-using System.Collections;
-using Windows.Devices.I2c;
-
-namespace JetsonPowerMgmtFirmware.INA219
+﻿namespace JetsonPowerMgmtFirmware.INA219
 {
+    using System;
+    using System.Collections;
+    using System.Text;
+    using Windows.Devices.I2c;
+
+    /// <summary>
+    /// Holds correct register locations for all registers in a INA219 device
+    /// </summary>
     public enum INA219Registers
     {
         Configuration = 0x00,
@@ -14,24 +17,28 @@ namespace JetsonPowerMgmtFirmware.INA219
         Current = 0x04,
         Calibration = 0x05
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class INA219Controller
     {
-        private readonly int INAStartAddress = 0x40;     //Jetson PWR MGMT Module starts from address 1000000 up to 1000005       
-        private readonly double ShuntResistance = 0.005;
-        private readonly int NominalVoltage = 12;
-        private readonly double MaxExpectedCurrent = 4.0;
+        private const int INAStartAddress = 0x40; //Jetson PWR MGMT Module starts from address 1000000 up to 1000005       
+        private const double ShuntResistance = 0.005;
+        private const int NominalVoltage = 12;
+        private const double MaxExpectedCurrent = 4.0;
         private ArrayList INADevices;
-        public int INACount { get; set; } = 0;
+
         /// <summary>
-        /// Creates list of all INA devices and configures them
+        /// Initializes a new instance of the <see cref="INA219Controller"/> class.
         /// </summary>
         /// <param name="i2cBus">Designated I2C bus that has only INA219 devices</param>
         public INA219Controller(string i2cBus)
         {
-            INADevices = new ArrayList();
+            this.INADevices = new ArrayList();
             int INAAddress = INAStartAddress;
-            bool IsRunning = true;
-            while (IsRunning)
+            bool isRunning = true;
+            while (isRunning)
             {
                 I2cDevice currentINA;
 
@@ -49,36 +56,36 @@ namespace JetsonPowerMgmtFirmware.INA219
 
                 if (BitConverter.ToInt16(readBuffer, 0) == 0x399)
                 {
-                    INADevices.Add(currentINA);
+                    this.INADevices.Add(currentINA);
                 }
                 else
                 {
-                    IsRunning = false;
+                    isRunning = false;
                 }
   
                 INAAddress++;
             }
             
-            foreach(I2cDevice INADevice in INADevices)
+            foreach (I2cDevice INADevice in this.INADevices)
             {
-                double MaxCurrent = NominalVoltage / ShuntResistance;
-                double CurrentLSB = Math.Round((MaxExpectedCurrent / 32767) * 1000000);
-                CurrentLSB /= 1000000;
+                double maxCurrent = NominalVoltage / ShuntResistance;
+                double currentLSB = Math.Round((MaxExpectedCurrent / 32767) * 1000000);
+                currentLSB /= 1000000;
 
-                float CalibrationReg = (float)Math.Truncate(0.04096 / (MaxCurrent * ShuntResistance));
+                float calibrationReg = (float)Math.Truncate(0.04096 / (maxCurrent * ShuntResistance));
                 
-                ushort ConfigurationReg = 0b001101110111111;
+                ushort configurationReg = 0b001101110111111;
 
                 byte[] writeBuffer = BitConverter.GetBytes((ushort)INA219Registers.Configuration);
                 INADevice.Write(writeBuffer);
 
-                writeBuffer = BitConverter.GetBytes(ConfigurationReg);
+                writeBuffer = BitConverter.GetBytes(configurationReg);
                 INADevice.Write(writeBuffer);
 
                 writeBuffer = BitConverter.GetBytes((ushort)INA219Registers.Calibration);
                 INADevice.Write(writeBuffer);
 
-                writeBuffer = BitConverter.GetBytes(CalibrationReg);
+                writeBuffer = BitConverter.GetBytes(calibrationReg);
                 INADevice.Write(writeBuffer);
             }
         }
@@ -87,27 +94,29 @@ namespace JetsonPowerMgmtFirmware.INA219
         {
             ArrayList currents = new ArrayList();
 
-            for (int i = 0; i<INADevices.Count; i++)
+            for (int i = 0; i < this.INADevices.Count; i++)
             {
-                currents.Add(GetCurrent(i));
+                currents.Add(this.GetCurrent(i));
             }
 
             return currents;
         }
+
         public ArrayList GetAllVoltages()
         {
             ArrayList voltages = new ArrayList();
 
-            for (int i = 0; i < INADevices.Count; i++)
+            for (int i = 0; i < this.INADevices.Count; i++)
             {
-                voltages.Add(GetVoltage(i));
+                voltages.Add(this.GetVoltage(i));
             }
 
             return voltages;
         }
+
         public float GetCurrent(int i)
         {
-            I2cDevice selectedINA = (I2cDevice)INADevices[i];
+            I2cDevice selectedINA = (I2cDevice)this.INADevices[i];
 
             byte[] writeBuffer = { 0 };
             byte[] readBuffer = { 0 };
@@ -119,9 +128,10 @@ namespace JetsonPowerMgmtFirmware.INA219
 
             return (float)BitConverter.ToDouble(readBuffer, 0);
         }
+
         public float GetVoltage(int i)
         {
-            I2cDevice selectedINA = (I2cDevice)INADevices[i];
+            I2cDevice selectedINA = (I2cDevice)this.INADevices[i];
 
             byte[] writeBuffer = { 0 };
             byte[] readBuffer = { 0 };
